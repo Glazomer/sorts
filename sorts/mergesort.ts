ef335555afd2abb412c638ffb1eed283e0ab3800
Glazomer <dan2000kr@gmail.com>
2020-11-14 16:41:18 +0300

    first version

--- sorts/mergesort.ts
+++ sorts/mergesort.ts
@@ -10,9 +10,26 @@ Implementation of in-place merge sort with stack.
 */
 
 module.exports = function MergeSort(arr: number[]): number[] {
-  for (let i = 2; i < arr.length; i *= 2) {
-    const temp = new Array(i);
-  }
+  const store = new Array(arr.length);
+  // Len in length of subsequences that getting sorted
+  for (let len = 2; len / 2 < arr.length; len *= 2) {
+    let i = 0; // Index i for filling sorted subsequences into store
+    for (let start = 0; start < arr.length; start += len) {
+      // Iterating over subsequences
+      const end = Math.min(start + len, arr.length),
+        middle = Math.min(start + len / 2, end);
 
-  return arr.sort((a, b) => a - b);
+      let l = start,
+        r = middle;
+      while (l < middle || r < end) {
+        if (r >= end || (l < middle && arr[l] < arr[r])) {
+          store[i++] = arr[l++];
+        } else {
+          store[i++] = arr[r++];
+        }
+      }
+    }
+    Object.assign(arr, store);
+  }
+  return arr;
 };
