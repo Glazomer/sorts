4a09122fbd14e7c23434c2ae0241df875c956197
Glazomer <dan2000kr@gmail.com>
2020-11-14 13:13:07 +0300

    attempt 1

--- sorts/mergesort.ts
+++ sorts/mergesort.ts
@@ -10,5 +10,9 @@ Implementation of in-place merge sort with stack.
 */
 
 module.exports = function MergeSort(arr: number[]): number[] {
+  for (let i = 2; i < arr.length; i *= 2) {
+    const temp = new Array(i);
+  }
+
   return arr.sort((a, b) => a - b);
 };
